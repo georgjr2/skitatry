df5d423ee6ff2bd4cfe4ecbe27823a36d4b68d9b
jmarticek <marticekjuraj@gmail.com>
2018-09-23 23:21:40 +0200

    Add working events

--- src/backend/controllers/calendar/show.js
+++ src/backend/controllers/calendar/show.js
@@ -1,3 +1,28 @@
+import Order from '../../models/order'
+
+
 export const handler = async (ctx) => {
-  await ctx.render('calendar')
+  const orders = await Order
+    .query()
+    .whereNotDeleted()
+
+
+  const events = orders.map((order) => ({
+    title: order.name,
+    start: order.from,
+    end: order.to,
+  }))
+  const calendarAttr = JSON.stringify({
+    header: {
+      right: 'agendaWeek month prev,next',
+      center: 'custom1',
+    },
+    firstDay: 1,
+    allDayDefault: true,
+    timeFormat: 'H:mm',
+    events,
+
+  })
+
+  await ctx.render('calendar', {titulok: 'ahoj', calendarAttr})
 }
