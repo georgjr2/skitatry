a82af2ba25207dfb83bd5b28913983b37ebc0c69
jmarticek <marticekjuraj@gmail.com>
2018-09-24 22:56:30 +0200

    events upgrade

--- src/backend/controllers/calendar/show.js
+++ src/backend/controllers/calendar/show.js
@@ -8,20 +8,28 @@ export const handler = async (ctx) => {
 
 
   const events = orders.map((order) => ({
-    title: order.name,
+    title: `${order.name} ${order.surname}`,
     start: order.from,
     end: order.to,
+    name: order.name,
+    surname: order.surname,
+    mail: order.mail,
+    age: order.age,
+    phone: order.phone,
+    confirmed: order.confirmed,
+    paid: order.paid,
   }))
+
   const calendarAttr = JSON.stringify({
     header: {
       right: 'agendaWeek month prev,next',
       center: 'custom1',
     },
+    selectable: true,
     firstDay: 1,
     allDayDefault: true,
     timeFormat: 'H:mm',
     events,
-
   })
 
   await ctx.render('calendar', {titulok: 'ahoj', calendarAttr})
